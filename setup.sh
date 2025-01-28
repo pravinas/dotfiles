d41d9a0230e11afc0da37b164a0ce8f83fbfe7cd
Samaratunga <pravi@ROSlab.bu.edu>
2025-01-28 17:06:57 -0500

    Added substantial setup to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
-ln -s .bashrc $HOME
-ln -s VimConf $HOME/.vim
-ln -s VimConf/.vimrc $HOME/.vimrc
+DOTFILE_DIR=$PWD
+
+ln -s $DOTFILE_DIR/.bashrc $HOME
+
+# Set up neovim
+cd /tmp
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x /tmp/nvim.appimage
+mv /tmp/nvim.appimage $HOME/.local/bin
+
+python3 -m pip install --user --upgrade pynvim # Add python support for YCM
+
+mkdir -p $HOME/.config/nvim
+ln -s $DOTFILE_DIR/init.vim $HOME/.config/nvim/init.vim
+ln -s $DOTFILE_DIR/.vim $HOME/.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -s $DOTFILE_DIR/.vimrc $HOME/.vimrc
+
+# Set up tmux
+mkdir -p $HOME/.config/tmux
+ln -s $DOTFILE_DIR/tmux.conf $HOME/.config/tmux/tmux.conf
+
+cd $DOTFILE_DIR
